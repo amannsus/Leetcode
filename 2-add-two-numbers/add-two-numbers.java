db43b4dac425169f8c5024cc2038a948e7575145
Aman Kumar Chaudhary <123335986+amannsus@users.noreply.github.com>
2026-08-19 16:33:12 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (19.85%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
-        ListNode dummy = new ListNode();
+        ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
         while(l1!=null ||l2!=null ||carry!=0){
             int sum = carry;
